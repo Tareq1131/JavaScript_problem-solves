e73b2a044570bdcd2a90bafed88b5aa536dc906b
Tareq1131 <tareqislam076@gmail.com>
2024-09-29 08:08:17 +0600

    Is Object Empty | Leetcode 2727 | JSON |

---
+++ LC2727.js
@@ -0,0 +1,11 @@
+
+var isEmpty = function(obj) {
+    if(Array.isArray(obj)){
+        return obj.length === 0
+    }
+    else{
+        let arr = Object.keys(obj)
+        return arr.length === 0
+    }
+    
+};
